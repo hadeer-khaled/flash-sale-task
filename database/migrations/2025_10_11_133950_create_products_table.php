72119eaecd36f1cb7b9217b8580f3ce78ca78423
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2025-10-13 13:48:04 +0300

    Add unique index to speed up queries and  prevents duplicate product code.

--- database/migrations/2025_10_11_133950_create_products_table.php
+++ database/migrations/2025_10_11_133950_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('code', 100);
+            $table->string('code', 100)->unique();
             $table->string('name', 100);
             $table->decimal('price', 10, 2);
             $table->integer('available_stock');
