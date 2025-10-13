df93bc13eb723534992f4457d4256e59d5d1b4c0
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2025-10-13 18:40:19 +0300

    Fix: correct table name in idempotency_logs migration

--- database/migrations/2025_10_11_180453_create_idempotency_logs_table.php
+++ database/migrations/2025_10_11_180453_create_idempotency_logs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('payment_logs', function (Blueprint $table) {
+        Schema::create('idempotency_logs', function (Blueprint $table) {
             $table->id();
             $table->string('key')->unique();
             $table->timestamps();
